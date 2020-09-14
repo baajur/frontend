248230c7c6457270cc35318e666bfd0a79825d7f
Botho <1258870+elbotho@users.noreply.github.com>
2020-09-14 14:28:08 +0200

    add main menu active state

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,7 +1,7 @@
 import { tint, shade } from 'polished'
 
 // base colors
-const brand = '#007ec1'
+const brand = '#007cbf' //was '#007ec1'
 const black = '#000'
 
 // use object notation to allow types to be infered
